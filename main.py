16de227e7a2d0a54036074c68a1b1a63c0cb06d3
kl <datesaver1234@gmail.com>
2021-05-26 02:32:41 +0900

    get params

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, jsonify, make_response
+import json
 app = Flask(__name__)
 
 
 @app.route("/api/v1/iconimage", methods=['GET'])
-def getHoge():
+def getData():
     # URLパラメータ
     params = request.args
     response = {}
-    print(request.get_data())
     if 'param' in params:
+        print(params.get('param'))
         response.setdefault('res', 'param is : ' + params.get('param'))
     return make_response(jsonify(response))
+
+
+@app.route("/api/v1/iconimage", methods=['POST'])
+def postData():
+    json_dict = request.get_json()
+    print(json_dict["image"])
 
 
 if __name__ == "__main__":
