ddc39c33ac9e63050f1472636427bbb5a857445c
kl <datesaver1234@gmail.com>
2021-05-26 03:10:49 +0900

    dastapi

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from flask import Flask, request, jsonify, make_response
-import json
-app = Flask(__name__)
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-@app.route("/api/v1/iconimage", methods=['GET'])
-def getData():
-    # URLパラメータ
-    params = request.args
-    response = {}
-    if 'param' in params:
-        print(params.get('param'))
-        response.setdefault('res', 'param is : ' + params.get('param'))
-    return make_response(jsonify(response))
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-@app.route("/api/v1/iconimage", methods=['POST'])
-def postData():
-    json_dict = request.get_json()
-    print(json_dict["image"])
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/hello_post")
+def post_hello(param: str):
+    """
+    postで返事する
+    """
+    if param:
+        message = f"[POST]hello, {param}!"
+    else:
+        message = f"[POST]hello, visitor!"
+
+    return {"message": message}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
